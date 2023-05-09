c56b2c90ac0194efc220e6d51e98adab292aa917
itsosmx <o.h1277@yahoo.com>
2023-05-09 21:13:13 +0300

    main

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "taskingo-3a7cd.firebaseapp.com",
-  databaseURL: "https://taskingo-3a7cd-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "taskingo-3a7cd",
-  storageBucket: "taskingo-3a7cd.appspot.com",
-  messagingSenderId: "901889173137",
-  appId: "1:901889173137:web:ec4549755289c694118130",
-  measurementId: "G-8P67T76EYF",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 initializeApp(firebaseConfig);
